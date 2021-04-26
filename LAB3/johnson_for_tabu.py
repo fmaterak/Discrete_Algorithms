333478b9c88be4f5e4fde17a6c9fb66b8a8d55da
fmaterak <62309735+fmaterak@users.noreply.github.com>
2021-04-26 03:24:02 +0200

    Add files via upload

---
+++ LAB3/johnson_for_tabu.py
@@ -0,0 +1,67 @@
+import numpy
+
+
+# read the data from file
+def read_from_file(filename):
+    with open(filename, "r") as fic:
+        my_nbm = int(fic.readline())
+        my_nbj = int(fic.readline())
+        my_p_ij = numpy.zeros((my_nbm, my_nbj))
+        for i in range(my_nbm):
+            p_i = fic.readline().split()
+            for j in range(my_nbj):
+                my_p_ij[i][j] = int(p_i[j])
+    return my_nbm, my_nbj, my_p_ij
+
+def read_file(filename):
+    with open(filename, "r") as fic:
+        line = fic.readline().split()
+        my_nbj = int(line[0])
+        my_nbm = int(line[1])
+        my_p_ij = numpy.zeros((my_nbj, my_nbm))
+        for i in range(my_nbj):
+            temp = fic.readline().split()
+            for j in range(my_nbm):
+                my_p_ij[i][j] = temp[j]
+    return my_nbm, my_nbj, my_p_ij.transpose()
+
+# calculate the c_ij table.
+def makespan(my_seq, processing_time, machines):
+    c_ij = numpy.zeros((machines, len(my_seq) + 1))
+
+    for j in range(1, len(my_seq) + 1):
+        c_ij[0][j] = c_ij[0][j - 1] + processing_time[0][my_seq[j - 1]]
+
+    for i in range(1, machines):
+        for j in range(1, len(my_seq) + 1):
+            c_ij[i][j] = max(c_ij[i - 1][j], c_ij[i][j - 1]) + processing_time[i][my_seq[j - 1]]
+    return c_ij
+
+
+def u(data, jobs):
+    my_u = []
+    for j in range(jobs):
+        if data[0][j] <= data[1][j]:
+            my_u.append(j)
+    return sorted(my_u, key=lambda x: data[0][x])
+
+
+# processing_time should be a two dimension array
+def v(data, jobs):
+    my_v = []
+    for j in range(jobs):
+        if data[0][j] > data[1][j]:
+            my_v.append(j)
+    return sorted(my_v, key=lambda x: data[1][x], reverse=True)
+
+
+def johnson(data, nb_machines, nb_jobs):
+    seq_current = u(data, nb_jobs) + v(data, nb_jobs) + [0]
+    return seq_current, makespan(seq_current, data, nb_machines)[nb_machines - 1][nb_jobs]
+
+
+# run Johnson
+def run():
+    machines, jobs, processing_time = read_file("test.txt")
+    seq, cmax = johnson(processing_time, machines, jobs)
+    return seq
---
+++ LAB3/tabu_search.py
@@ -0,0 +1,125 @@
+import math
+import random
+import numpy as np
+import johnson_for_tabu
+import NEH_for_tabu
+
+
+def read_from_file(filename):
+    f = open(filename, 'r')
+    line = f.readline().split()
+    nm_of_jobs = int(line[0])
+    nm_of_machines = int(line[1])
+    pij = np.zeros((nm_of_jobs + 1, nm_of_machines + 1))
+    for i in range(1, nm_of_jobs + 1):
+        temp = f.readline().split()
+        for j in range(1, nm_of_machines + 1):
+            pij[i][j] = int(temp[j - 1])
+    f.close()
+    return pij
+
+
+def count_cmax(sched, times):
+    tmp = [[None for _ in range(len(times[0]))] for _ in range(len(times))]
+    for i in range(0, len(times)):
+        for j in range(len(times[0])):
+            if i == 0:
+                if j == 0:
+                    tmp[i][j] = times[sched[i] - 1][j]
+                else:
+                    tmp[i][j] = (tmp[i][j - 1] + times[sched[i] - 1][j])
+            else:
+                if j == 0:
+                    tmp[i][j] = tmp[i - 1][j] + times[sched[i] - 1][j]
+                else:
+                    tmp[i][j] = max(tmp[i - 1][j], tmp[i][j - 1]) + times[sched[i] - 1][j]
+
+    return tmp[len(times) - 1][len(times[0]) - 1]
+
+
+def swap_neighbourhoods(schedule, neighbourhoods):
+    for i in range(0, len(schedule)):
+        temp_schedule = schedule[:]
+        for j in range(i + 1, len(schedule)):
+            temp_schedule[i], temp_schedule[j] = temp_schedule[j], temp_schedule[i]
+            if temp_schedule not in neighbourhoods:
+                neighbourhoods.append(temp_schedule[:])
+            temp_schedule[i], temp_schedule[j] = temp_schedule[j], temp_schedule[i]
+    return neighbourhoods
+
+
+def neighbourhoods_generator(schedule, function="swap"):
+    neighbourhoods_list = []
+    neighbourhoods_list = swap_neighbourhoods(schedule, neighbourhoods=neighbourhoods_list)
+    return neighbourhoods_list
+
+
+def best_neighbourhood(schedules, times, tabu):
+    cmin = math.inf
+    tmp = schedules[0]
+    for schedule in schedules:
+        if schedule not in tabu:
+            cmax = count_cmax(schedule, times)
+            if cmax < cmin:
+                cmin = cmax
+                tmp = schedule
+    if cmin == math.inf:
+        for schedule in tabu:
+            cmax = count_cmax(schedule, times)
+            if cmax < cmin:
+                cmin = cmax
+                tmp = schedule
+
+    return cmin, tmp
+
+
+# Generowanie rozwiazania poczatkowego
+def initialize_shedule(times, method="random"):
+    if method == "neh":
+        return NEH_for_tabu.run()
+    if method == "johnson":
+        return johnson_for_tabu.run()
+    if method == "random":
+        tmp = list(range(1, len(times) + 1))
+        random.shuffle(tmp)
+        return tmp
+    if method == "sequence":
+        return list(range(1, len(times) + 1))
+
+
+def make_search(times, tabu, max_tabu, current, best_cmax, best, method):
+    neighbourhoods = neighbourhoods_generator(current, function=method)
+    tmp_tabu = tabu[-max_tabu:]
+    current_cmax, current = best_neighbourhood(neighbourhoods, times, tmp_tabu[:])
+    tabu.append(current)
+    if current_cmax < best_cmax:
+        best = current
+        best_cmax = current_cmax
+
+    return best, best_cmax, tabu, current
+
+
+def tabu_search(times, max_tabu=20, iterations=500, init_function="random", neighbourhoods_function="swap"):
+    schedule = initialize_shedule(times, method=init_function)
+    best = schedule
+    best_cmax = count_cmax(schedule, times)
+    tabu = [schedule]
+    current = schedule[:]
+    for i in range(0, iterations):
+        tmp = make_search(times, tabu[:], max_tabu, current, best_cmax, best, neighbourhoods_function)
+        best = tmp[0]
+        best_cmax = tmp[1]
+        tabu = tmp[2]
+        current = tmp[3]
+
+    return best, best_cmax
+
+def run():
+    data = read_from_file("test.txt")
+    best, best_cmax = tabu_search(data)
+    return best, best_cmax
+
+if __name__ == "__main__":
+    best,best_cmax = run()
+    print(best_cmax)
+    print(best)
